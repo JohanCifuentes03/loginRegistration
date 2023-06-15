20e9176815b0ad80e3c120a1a7e2e451b4fda942
johan <johancfuentes03@gmail.com>
2023-06-15 14:18:32 -0500

    working version

---
+++ src/main/java/com/jsc/data/UserDAO.java
@@ -0,0 +1,90 @@
+package com.jsc.data;
+
+import com.jsc.entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private Connection connection;
+
+    public UserDAO() throws SQLException {
+        this.connection = DBConnection.getConnection();
+    }
+
+    public int insert(User user) throws SQLException {
+        String query = "INSERT INTO users (username, email, phone, password) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPhone());
+            statement.setString(4, user.getPassword());
+            return statement.executeUpdate();
+        }
+    }
+
+    public int update(User user) throws SQLException {
+        String query = "UPDATE users SET username = ?, email = ?, phone = ?, password = ? WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPhone());
+            statement.setString(4, user.getPassword());
+            statement.setLong(5, user.getId());
+            return statement.executeUpdate();
+        }
+    }
+
+    public int delete(long id) throws SQLException {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1, id);
+            return statement.executeUpdate();
+        }
+    }
+
+    public User findById(long id) throws SQLException {
+        String query = "SELECT * FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapResultSetToUser(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<User> findAll() throws SQLException {
+        String query = "SELECT * FROM users";
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            List<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                users.add(mapResultSetToUser(resultSet));
+            }
+            return users;
+        }
+    }
+
+    private User mapResultSetToUser(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("username"));
+        user.setEmail(resultSet.getString("email"));
+        user.setPhone(resultSet.getString("phone"));
+        user.setPassword(resultSet.getString("password"));
+        return user;
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
--- src/main/java/com/jsc/registration/RegistrationServlet.java
+++ src/main/java/com/jsc/registration/RegistrationServlet.java
@@ -1,40 +1,51 @@
 package com.jsc.registration;
 
+import com.jsc.data.UserDAO;
+import com.jsc.entity.User;
 
-import com.jsc.registration.validations.UserRegistration;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-
-
+import java.sql.SQLException;
 
 @WebServlet("/RegistrationServlet")
 public class RegistrationServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-
-        RequestDispatcher dispatcher;
+    private void registerUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = null;
         String userName     = request.getParameter("name");
         String userEmail    = request.getParameter("email");
         String userPassword = request.getParameter("pass");
         String userMobile   = request.getParameter("contact");
 
-        int rows   = UserRegistration.registerUsers(userName,userEmail,userPassword,userMobile);
-        dispatcher = request.getRequestDispatcher("registration.jsp");
-        if (rows > 0){
-            request.setAttribute("status", "success");
-        }else {
+        User user = new User(userName, userEmail, userMobile, userPassword);
+
+        try {
+            UserDAO userDAO  = new UserDAO();
+            int rowsAffected = userDAO.insert(user);
+            dispatcher = request.getRequestDispatcher("registration.jsp");
+            if (rowsAffected > 0) {
+                request.setAttribute("status", "success");
+            } else {
+                request.setAttribute("status", "failed");
+            }
+            dispatcher.forward(request, response);
+            userDAO.closeConnection();
+        } catch (SQLException e) {
             request.setAttribute("status", "failed");
+            request.setAttribute("errorMessage", "Error in database operation: " + e.getMessage());
+            dispatcher = request.getRequestDispatcher("registration.jsp");
+            dispatcher.forward(request, response);
         }
+    }
 
-        dispatcher.forward(request, response);
-
-        }
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        registerUser(request, response);
+    }
 
 }
